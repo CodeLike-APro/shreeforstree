ca489b49082815982215fdf8a8506c4651efcdc5
CodeLike-APro <minecraft009111@gmail.com>
2026-06-06 11:47:57 +0530

    Add Cloudinary configuration and dependencies; remove unused SVG files

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: `/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/**`,
+      },
+    ],
+  },
 };
 // next.config.js
 module.exports = {
   allowedDevOrigins: ["192.168.1.7"],
 };
+
 export default nextConfig;
