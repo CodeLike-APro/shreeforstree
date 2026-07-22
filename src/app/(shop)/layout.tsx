7a892c4c52bf4a1d95611daa6504ade6e71c3200
CodeLike-APro <minecraft009111@gmail.com>
2026-07-22 21:14:07 +0530

    feat: implement shop page structure, responsive headers, and interactive search functionality

---
+++ src/app/(shop)/layout.tsx
@@ -0,0 +1,23 @@
+import dynamic from "next/dynamic";
+import { headers } from "next/headers";
+
+const HeaderDesktop = dynamic(() => import("@/components/shop/HeaderDesktop"));
+const HeaderMobile = dynamic(() => import("@/components/shop/HeaderMobile"));
+
+export default async function ShopLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const headerList = await headers();
+  const deviceType = headerList.get("x-device-type");
+
+  return (
+    <section className="min-h-screen flex flex-1 flex-col">
+      <header>
+        {deviceType === "mobile" ? <HeaderMobile /> : <HeaderDesktop />}
+      </header>
+      <main>{children}</main>
+    </section>
+  );
+}
---
+++ src/app/(shop)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="text-8xl font-display font-bold h-screen w-full text-center items-center flex justify-center">
+      SHREEFORSTREE
+    </div>
+  );
+}
