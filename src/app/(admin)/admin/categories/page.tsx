fa00f99726b5df13b0d7ceef6a6f8596ebe08837
CodeLike-APro <minecraft009111@gmail.com>
2026-07-26 15:22:00 +0530

    feat: add new pages for categories, notifications, orders, products, settings, users, and shop sections

---
+++ src/app/(admin)/admin/categories/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className=" h-screen w-full text-center flex flex-col items-center  justify-center">
+      <h1 className="text-4xl md:text-8xl font-display md:text-ink font-bold">
+        Categories
+      </h1>
+    </div>
+  );
+}
