a889d7140cc3e6001a9350d8d4b5e7331893fa9f
CodeLike-APro <minecraft009111@gmail.com>
2026-09-02 13:16:04 +0530

    refactor: update Checkout component to manage address display state and remove AddressModal

--- src/app/(shop)/checkout/page.tsx
+++ src/app/(shop)/checkout/page.tsx
@@ -1,11 +1,10 @@
 "use client";
 
-import AddressModal from "@/components/shop/Addresses/AddressModal";
 import AllAddresses from "@/components/shop/Addresses/AllAddresses";
 import { useState } from "react";
 
 export default function Checkout() {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isExpanded, setIsExpanded] = useState(false);
 
   return (
     <div className="flex flex-col items-start gap-2 px-10 py-10">
@@ -16,24 +15,26 @@ export default function Checkout() {
         </p>
       </div>
 
-      <div className="flex w-full items-center justify-between">
-        <h4 className="font-label font-bold tracking-wide">Delivery Address</h4>
-        <button
-          onClick={() => setIsOpen(true)}
-          className="font-label text-md border-ink bg-paper text-ink hover:bg-ink hover:text-paper focus:ring-rose-gold rounded-md border px-4 py-1.5 transition-colors duration-150 focus:border-transparent focus:ring-2 focus:outline-none"
-        >
-          Change address
-        </button>
-      </div>
-      <div className="relative">
-        <AddressModal
-          title="new"
-          isOpen={isOpen}
-          onClose={() => setIsOpen(false)}
-        />
-      </div>
-      <div className="w-full">
-        <AllAddresses />
+      <div className="relative flex w-full flex-col items-center justify-between gap-2">
+        <div className="flex h-10 w-full items-center justify-between">
+          <h4 className="font-label font-bold tracking-wide">
+            Delivery Address
+          </h4>
+          {!isExpanded && (
+            <button
+              onClick={() => !isExpanded && setIsExpanded(!isExpanded)}
+              className="font-label text-md border-ink bg-paper text-ink hover:bg-ink hover:text-paper focus:ring-rose-gold rounded-md border px-4 py-1.5 transition-colors duration-150 focus:border-transparent focus:ring-2 focus:outline-none"
+            >
+              Change Address
+            </button>
+          )}
+        </div>
+        <div className="w-full">
+          <AllAddresses
+            expand={isExpanded}
+            onCollapse={() => setIsExpanded(false)}
+          />
+        </div>
       </div>
     </div>
   );
