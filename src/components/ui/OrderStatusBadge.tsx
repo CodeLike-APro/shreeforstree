fabc7d945a45349affe0ff9bf0221e62c8701bd3
CodeLike-APro <minecraft009111@gmail.com>
2026-09-16 19:54:06 +0530

    feat: add OrderStatusBadge component for displaying order and payment statuses

---
+++ src/components/ui/OrderStatusBadge.tsx
@@ -0,0 +1,64 @@
+import { orderStatusEnum, paymentStatusEnum } from "@/lib/db/schema";
+
+type orderStatuses = typeof orderStatusEnum.enumValues;
+type paymentStatuses = typeof paymentStatusEnum.enumValues;
+
+type OrderStatusColor = {
+  status: orderStatuses[number];
+  bgColor: string;
+  textColor: string;
+};
+
+const baseClasses = "rounded-md px-2 py-1 text-xs tracking-widest  uppercase";
+const orderStatusColors: OrderStatusColor[] = [
+  { status: "not_placed", bgColor: "bg-ink-15", textColor: "text-ink" },
+  { status: "placed", bgColor: "bg-dusk/20", textColor: "text-dusk" },
+  {
+    status: "confirmed",
+    bgColor: "bg-plum/20",
+    textColor: "text-plum",
+  },
+  {
+    status: "shipped",
+    bgColor: "bg-terracotta/20",
+    textColor: "text-terracotta",
+  },
+  { status: "delivered", bgColor: "bg-sage/20", textColor: "text-sage" },
+  { status: "cancelled", bgColor: "bg-crimson/20", textColor: "text-crimson" },
+  {
+    status: "returned",
+    bgColor: "bg-crimson-dark/20",
+    textColor: "text-crimson-dark",
+  },
+];
+
+export default function OrderStatusBadge({
+  kind,
+  status,
+}:
+  | {
+      kind: "order";
+      status: orderStatuses[number];
+    }
+  | {
+      kind: "payment";
+      status: paymentStatuses[number];
+    }) {
+  const correctStatus =
+    kind === "order" && status.toLowerCase() === "not_placed"
+      ? "not placed"
+      : status.toLowerCase();
+  return (
+    <div>
+      {kind === "order" && (
+        <p
+          className={`${baseClasses} ${
+            orderStatusColors.find((s) => s.status === status)?.bgColor
+          } ${orderStatusColors.find((s) => s.status === status)?.textColor}`}
+        >
+          {correctStatus}
+        </p>
+      )}
+    </div>
+  );
+}
